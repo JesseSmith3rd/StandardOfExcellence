6b72a5ab3cd634e30645a48be47ef9258896755e
Jesse <jessesmith3rd@outlook.com>
2025-02-26 01:01:19 -0500

    normalize line endings

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,16 +1,15 @@
-import Link from "next/link";
+
 
 const Navbar = () => {
   return (
+    
     <nav className="bg-blue-600 p-4">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <h1 className="text-white text-2xl font-bold">Standard of Excellence</h1>
-
+       
         {/* Navigation Links */}
-        <ul className="flex space-x-6">
-   
-         
+        <ul className="flex space-x-6"> 
         </ul>
       </div>
     </nav>
